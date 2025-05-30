6e98540b8a3c2a9eb5f17267887a290cb1353348
Sophia Na (나소연) <devsophia029@gmail.com>
2025-05-30 23:48:25 +0900

    feat: add TextDropdown

---
+++ src/components/TextField/TextDropdown.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import styles from './TextField.module.css';
+import arrowUp from '../../assets/icons/arrow_top.svg';
+import arrowDown from '../../assets/icons/arrow_down.svg';
+
+function TextDropdown({ value, options, onChange, error }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleSelect = option => {
+		onChange(option);
+		setIsOpen(false);
+	};
+
+	return (
+		<div className={`${styles.container} ${error ? styles.error : ''}`}>
+			{/*클래스네임 다시 구분 필요함.*/}
+			<div className={styles.dropdownHeader} onClick={() => setIsOpen(!isOpen)}>
+				{value}
+				<span className={styles.arrow}>
+					{isOpen ? <img src={arrowUp} /> : <img src={arrowDown} />}
+				</span>
+			</div>
+			{isOpen && (
+				<ul className={styles.dropdownList}>
+					{options.map((option, index) => (
+						<li key={index} onClick={() => handleSelect(option)}>
+							{option}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+}
+
+export default TextDropdown;
