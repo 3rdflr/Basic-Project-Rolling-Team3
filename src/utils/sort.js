87072fba8bddbcc71bdf6783adc9d422c8f9e3b5
3rdflr <3rdflrhtl@gmail.com>
2025-05-31 15:55:11 +0900

    ADD api useage

---
+++ src/utils/sort.js
@@ -0,0 +1,11 @@
+export const sortResent = data => {
+	const sortedData = data
+		.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+		.slice(0, 10);
+	return sortedData;
+};
+
+export const sortHot = data => {
+	const sortedData = data.sort((a, b) => b.messageCount - a.messageCount).slice(0, 10);
+	return sortedData;
+};
