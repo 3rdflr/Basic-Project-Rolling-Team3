d5669c971fc79972474c6a5d7adc71bca992dca5
Sophia Na (나소연) <devsophia029@gmail.com>
2025-06-01 22:31:40 +0900

    feat: implement TextDropdown component

--- src/components/TextField/TextDropdown.jsx
+++ src/components/TextField/TextDropdown.jsx
@@ -1,21 +1,33 @@
-import { useState } from 'react';
-import styles from './TextField.module.css';
+import { useRef, useState, useEffect } from 'react';
+import styles from './TextDropdown.module.css';
 import arrowUp from '../../assets/icons/arrow_top.svg';
 import arrowDown from '../../assets/icons/arrow_down.svg';
 
 function TextDropdown({ value, options, onChange, error }) {
 	const [isOpen, setIsOpen] = useState(false);
+	const dropdownRef = useRef(null);
 
 	const handleSelect = option => {
 		onChange(option);
 		setIsOpen(false);
 	};
 
+	useEffect(() => {
+		const handleClickOutside = event => {
+			if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+				setIsOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
 	return (
-		<div className="relative">
-			{/*클래스네임 다시 구분 필요함.*/}
+		<div className={styles.wrapper} ref={dropdownRef}>
 			<div className={styles.container} onClick={() => setIsOpen(!isOpen)}>
-				{value}
+				<p className={styles.value}>{value}</p>
 				<span className={styles.arrow}>
 					{isOpen ? <img src={arrowUp} /> : <img src={arrowDown} />}
 				</span>
@@ -24,7 +36,7 @@ function TextDropdown({ value, options, onChange, error }) {
 				<ul className={styles.dropdownList}>
 					{options.map((option, index) => (
 						<li key={index} onClick={() => handleSelect(option)}>
-							{option}
+							<p className={styles.dropdownOption}>{option}</p>
 						</li>
 					))}
 				</ul>
