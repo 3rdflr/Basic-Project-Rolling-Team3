cd55a7db664d775951e22bbd7c5b52013a7cc04d
User <https://github.com/hong0121/plpl.git>
2025-06-01 20:28:46 +0900

    FEAT : Add KakaoTalk share feature with SDK initialization

--- src/components/buttons/ShareButton/ShareButton.jsx
+++ src/components/buttons/ShareButton/ShareButton.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { IoShareOutline } from 'react-icons/io5';
 import styles from './ShareButton.module.css';
+import { KAKAO_APP_KEY } from '../../../constants/endPoints';
 
 const ShareButton = () => {
 	const [open, setOpen] = useState(false);
@@ -18,6 +19,26 @@ const ShareButton = () => {
 		return () => document.removeEventListener('mousedown', handleClickOutside);
 	}, []);
 
+	useEffect(() => {
+		if (window.Kakao && !window.Kakao.isInitialized()) {
+			window.Kakao.init(KAKAO_APP_KEY);
+			console.log('카카오 SDK 초기화 완료');
+		}
+	}, []);
+
+	const handleKakaoShare = () => {
+		if (window.Kakao) {
+			window.Kakao.Share.sendDefault({
+				objectType: 'text',
+				text: '이것은 테스트 공유 메시지입니다!',
+				link: {
+					mobileWebUrl: window.location.href,
+					webUrl: window.location.href,
+				},
+			});
+		}
+	};
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.sharebutton} onClick={toggleDropdown}>
@@ -25,7 +46,7 @@ const ShareButton = () => {
 			</div>
 			{open && (
 				<ul className={styles.dropdawn}>
-					<li>카카오톡 공유</li>
+					<li onClick={handleKakaoShare}>카카오톡 공유</li>
 					<li>URL 공유</li>
 				</ul>
 			)}
