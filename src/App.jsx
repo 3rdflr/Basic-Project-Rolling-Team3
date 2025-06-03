33f1c3bb0b602383cd7673c86faacecf3bc5d869
3rdflr <3rdflrhtl@gmail.com>
2025-06-03 15:34:27 +0900

    ADD Route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage/LandingPage';
+import ListPage from './pages/ListPage/ListPage';
 import WritePaper from './pages/WritePaper/WritePaper';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/additem" element={<ListPage />} />
 				<Route path="/post/:id/message" element={<WritePaper />} />
 			</Routes>
 		</BrowserRouter>
