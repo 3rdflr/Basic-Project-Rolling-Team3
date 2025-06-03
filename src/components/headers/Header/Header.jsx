1828ce30d4c9b27bd4621c41736d1f8c728dea6a
3rdflr <3rdflrhtl@gmail.com>
2025-06-03 09:23:48 +0900

    Fix Header for form

--- src/components/headers/Header/Header.jsx
+++ src/components/headers/Header/Header.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router';
 import icon from '../../../assets/icons/logo.svg';
 import styles from './Header.module.css';
 
-function Header() {
+function Header({ isForm }) {
 	return (
 		<div className={styles.header}>
 			<div className={styles.content}>
 				<Link to={'/'}>
 					<img src={icon} alt="롤링 로고" />
 				</Link>
-				<Button style={'button'} linkTo={'/post'} text={'롤링 페이퍼 만들기'} />
+				{isForm && <Button style={'button'} linkTo={'/post'} text={'롤링 페이퍼 만들기'} />}
 			</div>
 		</div>
 	);
