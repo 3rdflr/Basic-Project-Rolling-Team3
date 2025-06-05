54faa53ec87bc726ccb86457e6eeea5d355918e6
Sophia Na (나소연) <devsophia029@gmail.com>
2025-06-05 22:04:34 +0900

    style: add disabled status

--- src/components/buttons/BackgroundSelector/BackgroundSelector.jsx
+++ src/components/buttons/BackgroundSelector/BackgroundSelector.jsx
@@ -1,24 +1,39 @@
-import { useState } from 'react';
-// import useGetData from '../../../hooks/useGetData';
+import { useState, useEffect } from 'react';
+import { imagesAPI } from '../../../api';
+import useFetch from '../../../hooks/useFetch';
 import styles from './BackgroundSelector.module.css';
 import checkIcon from '../../../assets/icons/check.png';
 
-function BackgroundSelector() {
-	const [selectedTab, setSelectedTab] = useState('color');
-	// const { data: imageData, loading, error } = useGetData('BACKGROUND_IMGS', 'GET');
+function BackgroundSelector({ onSelect, selectedTab, setSelectedTab }) {
 	const [selectedColor, setSelectedColor] = useState(null);
 	const [selectedImage, setSelectedImage] = useState(null);
+	const [imageData, setImageData] = useState([]);
+
+	const [isLoading, error, getImages] = useFetch(imagesAPI.getBackgrounds);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getImages();
+				setImageData(data);
+			} catch (error) {
+				console.error('이미지 불러오기 실패:', error);
+			}
+		};
+		fetchData();
+	}, [getImages]);
 
 	return (
 		<div className={styles.container}>
 			<div>
 				<button
+					type="button"
 					className={selectedTab === 'color' ? styles.activeTab : styles.inactiveTab}
 					onClick={() => setSelectedTab('color')}
 				>
 					컬러
 				</button>
 				<button
+					type="button"
 					className={selectedTab === 'image' ? styles.activeTab : styles.inactiveTab}
 					onClick={() => setSelectedTab('image')}
 				>
@@ -36,6 +51,7 @@ function BackgroundSelector() {
 							onClick={() => {
 								setSelectedColor(idx);
 								setSelectedImage(null);
+								onSelect({ backgroundColor: color, backgroundImageURL: '' });
 							}}
 						>
 							{selectedColor === idx && (
@@ -47,15 +63,21 @@ function BackgroundSelector() {
 			)}
 			{selectedTab === 'image' && (
 				<div className={styles.imagePalette}>
-					{loading && <p>Loading...</p>}
-					{error && <p>Error loading images</p>}
-					{imageData?.imageUrls?.map((url, idx) => (
+					{/* 추후 수정 필요 */}
+					{isLoading && <p>이미지를 불러오는 중입니다...</p>}
+					{error && <p>이미지를 불러오는데 실패했습니다.</p>}
+					{imageData.map((url, idx) => (
 						<div
 							key={idx}
 							className={`${styles.imageWrapper} ${selectedImage === idx ? styles.selected : ''}`}
 							onClick={() => {
 								setSelectedImage(idx);
 								setSelectedColor(null);
+								onSelect(prev => ({
+									...prev,
+									backgroundColor: 'beige',
+									backgroundImageURL: imageData[idx],
+								}));
 							}}
 						>
 							<img className={styles.image} src={url} alt={`Image ${idx}`} />
--- src/components/buttons/Button/Button.jsx
+++ src/components/buttons/Button/Button.jsx
@@ -2,9 +2,13 @@ import { Link } from 'react-router';
 
 import styles from './Button.module.css';
 
-function Button({ type = 'button', style, text, linkTo }) {
+function Button({ type = 'button', style, text, linkTo, disabled = false }) {
 	return (
-		<button type={type} className={styles[style]}>
+		<button
+			type={type}
+			className={`${styles[style]} ${disabled ? styles.disabled : ''}`}
+			disabled={disabled}
+		>
 			{type === 'submit' ? <span>{text}</span> : <Link to={linkTo}>{text}</Link>}
 		</button>
 	);
