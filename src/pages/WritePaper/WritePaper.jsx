b1b397ff1736b80c4f1945680614537747088117
Sophia Na (나소연) <devsophia029@gmail.com>
2025-06-05 22:04:34 +0900

    style: add disabled status

--- src/pages/WritePaper/WritePaper.jsx
+++ src/pages/WritePaper/WritePaper.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import Header from '../../components/headers/Header/Header';
+// import Header from '../../components/headers/Header/Header';
 import TextInput from '../../components/TextField/TextInput';
 import BackgroundSelector from '../../components/buttons/BackgroundSelector/BackgroundSelector';
 import Button from '../../components/buttons/Button/Button';
@@ -8,43 +8,58 @@ import styles from './WritePaper.module.css';
 
 function WritePaper() {
 	const [name, setName] = useState('');
-	const [backgroundColor, setBackgroundColor] = useState('');
-	const [backgroundImageURL, setBackgroundImageURL] = useState('');
+	const [selectedTab, setSelectedTab] = useState('color');
+	const [selectedValue, setSelectedValue] = useState({
+		backgroundColor: 'beige',
+		backgroundImageURL: '',
+	});
 
 	const handleSubmit = async e => {
 		e.preventDefault();
 
 		const generationTeam = '16-3';
-		const teamId = '3';
 		const formData = {
-			team: teamId,
+			team: '16-3',
 			name: name.trim(),
 		};
-
-		if (backgroundColor) {
-			formData.backgroundColor = backgroundColor;
+		if (selectedValue.backgroundImageURL) {
+			formData.backgroundImageURL = selectedValue.backgroundImageURL;
+			formData.backgroundColor = null;
+		} else if (selectedValue.backgroundColor) {
+			formData.backgroundColor = selectedValue.backgroundColor || 'beige';
+			formData.backgroundImageURL = null;
+		} else {
+			if (selectedTab === 'image') {
+				formData.backgroundImageURL = 'https://picsum.photos/id/683/3840/2160';
+				formData.backgroundColor = null;
+			} else if (selectedTab === 'color') {
+				formData.backgroundColor = 'beige';
+				formData.backgroundImageURL = null;
+			}
 		}
 
-		if (backgroundImageURL) {
-			formData.backgroundImageURL = backgroundImageURL;
-		}
+		// if (selectedTab === 'image') {
+		// 	formData.backgroundColor = null;
+		// } else if (selectedTab === 'color') {
+		// 	formData.backgroundImageURL = null;
+		// }
+
 		try {
-			// POST 요청 보내기(api 변수 수정시 수정할 것!)
 			const response = await axios.post(
-				`https://rolling-api.vercel.app/${generationTeam}/${teamId}/recipients/`,
+				`https://rolling-api.vercel.app/${generationTeam}/recipients/`,
 				formData
 			);
 			console.log('성공:', response.data);
-			// TODO: 성공 시 알림, 페이지 이동, 토스트 메시지 등 처리(토스트 연결 가능한지 회의해봐야함)
 		} catch (error) {
 			console.error('에러:', error);
-			// TODO: 에러 토스트 띄우기 or 메시지 처리
 		}
+
+		console.log('formData', formData);
 	};
 
 	return (
 		<>
-			<Header isForm={false} />
+			{/* <Header isForm={false} /> */}
 			<form onSubmit={handleSubmit} className={styles.form}>
 				<h2 className={styles.h2}>To</h2>
 				<TextInput
@@ -57,9 +72,9 @@ function WritePaper() {
 				<p className={styles.p}>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
 				<div>
 					<BackgroundSelector
-						className={styles.selector}
-						onSelectColor={setBackgroundColor}
-						onSelectImage={setBackgroundImageURL}
+						selectedTab={selectedTab}
+						setSelectedTab={setSelectedTab}
+						onSelect={selectedValue => setSelectedValue(selectedValue)}
 					/>
 				</div>
 				<div>
