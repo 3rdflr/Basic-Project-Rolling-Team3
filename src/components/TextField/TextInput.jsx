f6f05ee3f25b3f64f219d048d828389bfb13eac3
Sophia Na (나소연) <devsophia029@gmail.com>
2025-06-05 22:06:06 +0900

    style: add error and errormessage class

--- src/components/TextField/TextInput.jsx
+++ src/components/TextField/TextInput.jsx
@@ -1,16 +1,17 @@
 import styles from './TextInput.module.css';
 
-function TextInput({ type, value, placeholder, onChange, error }) {
+function TextInput({ type, value, placeholder, onChange, error, errorMessage, onBlur }) {
 	return (
 		<div>
 			<input
-				className={styles.container}
+				className={`${styles.container} ${error && styles.error}`}
 				type={type}
 				value={value}
 				placeholder={placeholder}
 				onChange={onChange}
+				onBlur={onBlur}
 			/>
-			{error && <div className={styles.error}>유효성 검사 글귀 들어가는 곳</div>}
+			{error && <div className={styles.errorMessage}>{errorMessage}</div>}
 		</div>
 	);
 }
