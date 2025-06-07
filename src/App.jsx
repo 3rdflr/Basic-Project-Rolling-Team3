08526e9b3612b5aa2a88b601111363d8e5ab1f04
3rdflr <3rdflrhtl@gmail.com>
2025-06-07 16:14:57 +0900

    FIX routes in APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage';
+import ListPage from './pages/ListPage/ListPage.jsx';
 import WritePaper from './pages/WritePaper/WritePaper';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<WritePaper />} />
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/list" element={<ListPage />} />
+				<Route path="/post" element={<WritePaper />} />
 			</Routes>
 		</BrowserRouter>
 	);
