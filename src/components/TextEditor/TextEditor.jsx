54ba298190bfd4b20e64cc67c05281b145fdb5c9
Sophia Na (나소연) <devsophia029@gmail.com>
2025-06-10 08:59:38 +0900

    WIP: add TextEditor

---
+++ src/components/TextEditor/TextEditor.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css'; // 에디터 스타일
+
+function TextEditor({ value, onChange }) {
+	const modules = {
+		toolbar: [
+			['bold', 'italic', 'underline'],
+			[{ list: 'bullet' }, { list: 'ordered' }],
+			[{ color: [] }],
+			[{ align: [] }],
+		],
+	};
+
+	const formats = ['bold', 'italic', 'underline', 'list', 'bullet', 'ordered', 'color', 'align'];
+
+	return (
+		<div>
+			<ReactQuill
+				value={value}
+				onChange={onChange}
+				modules={modules}
+				formats={formats}
+				placeholder="내용을 입력해 주세요."
+			/>
+		</div>
+	);
+}
+
+export default TextEditor;
