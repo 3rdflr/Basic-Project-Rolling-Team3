bdf9ebc1a1cac34a87b8f06289dd0a20da7479b2
hong0121 <94957076+hong0121@users.noreply.github.com>
2025-06-10 08:56:14 +0900

    FEAT Modal EmojiButton & UI refactoring & implements custom hook (#27)
    * FEAT : add AddEmojiButton component
    
    * FEAT : add modal componet
    
    * REFACTORING : Implement global toggle logic & useClickOutside hook
    
    * REFACTORING : fix UI & implement utils
    
    * feat : Apply absolute import paths

---
+++ src/components/buttons/AddEmojiButton/AddEmojiButton.jsx
@@ -0,0 +1,38 @@
+import EmojiPicker from 'emoji-picker-react';
+import { useState } from 'react';
+import { recipientsAPI } from '../../../api';
+import AddEmojiIcon from '../../../assets/icons/AddEmojiIcon.svg';
+
+import styles from './AddEmojiButton.module.css';
+
+const AddEmojiButton = ({ recipientId, onReactionAdded, isOpen, toggleOpen }) => {
+	const handleEmojiClick = async emojiData => {
+		try {
+			const body = {
+				emoji: emojiData.emoji,
+				type: 'increase',
+			};
+			await recipientsAPI.createRecipientsReaction(recipientId, body);
+
+			onReactionAdded?.();
+			setOpenToggle(false);
+		} catch (error) {
+			console.error('이모지 추가 실패:', error);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<button className={styles.addButton} onClick={toggleOpen}>
+				<img src={AddEmojiIcon} alt="이모지 추가 아이콘" />
+			</button>
+			{isOpen && (
+				<div className={styles.pickerWrapper}>
+					<EmojiPicker onEmojiClick={handleEmojiClick} />
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default AddEmojiButton;
