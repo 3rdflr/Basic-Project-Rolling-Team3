f7b17f86405e9f6c08f9de684dbc8def81be9042
Sophia Na (나소연) <devsophia029@gmail.com>
2025-06-10 21:19:37 +0900

    feat: add wideButton class to Button component

--- src/components/buttons/Button/Button.jsx
+++ src/components/buttons/Button/Button.jsx
@@ -1,10 +1,20 @@
 import styles from './Button.module.css';
 
-function Button({ type = 'button', classStyle, children, linkTo, disabled = false, onClick }) {
+function Button({
+	type = 'button',
+	classStyle,
+	extraClass,
+	children,
+	linkTo,
+	disabled = false,
+	onClick,
+}) {
 	return (
 		<button
 			type={type}
-			className={`${styles[classStyle]} ${disabled ? styles.disabled : ''}`}
+			className={`${styles[classStyle]} ${extraClass ? styles[extraClass] : ''} ${
+				disabled ? styles.disabled : ''
+			}`}
 			disabled={disabled}
 			onClick={onClick}
 		>
